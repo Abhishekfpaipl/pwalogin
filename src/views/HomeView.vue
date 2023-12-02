fec5599dbc764730f0ba4bc7cbfd0ff886e71ac9
Abhishekfpaipl <131144777+Abhishekfpaipl@users.noreply.github.com>
2023-12-02 15:24:56 +0530

    added install alert

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-if="showInstallPopup" class="install-popup">
+      <div class="install-popup-content">
+        <p>Do you want to install this app?</p>
+        <button @click="installApp">Install</button>
+        <button @click="dismissInstall">Dismiss</button>
+      </div>
+    </div>
+
     <HelloWorld msg="Welcome to Your Pwa Login Test" />
 
     <router-link to="/login" class="btn btn-primary">Login</router-link>
@@ -16,5 +24,79 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      deferredPrompt: null,
+      showInstallButton: false,
+      showInstallPopup: false,
+      noti: []
+    };
+  },
+  created() {
+    window.addEventListener('beforeinstallprompt', this.handleInstallPrompt);
+  },
+  unmounted() {
+    window.removeEventListener('beforeinstallprompt', this.handleInstallPrompt);
+  },
+  methods: {
+    handleInstallPrompt(event) {
+
+      // Prevent the default behavior to show the browser's install prompt
+      event.preventDefault();
+
+      // Store the event for later use
+      this.deferredPrompt = event;
+
+      // Show a browser-style alert immediately
+      this.showInstallAlert();
+    },
+    showInstallAlert() {
+      // Set the flag to true to show the install popup
+      this.showInstallPopup = true;
+    },
+    installApp() {
+      // If the user clicks "Install," show the browser's install prompt
+      this.deferredPrompt.prompt();
+
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+
+        // Reset the deferredPrompt
+        this.deferredPrompt = null;
+
+        // Close the install popup
+        this.showInstallPopup = false;
+      });
+    },
+    dismissInstall() {
+      this.showInstallPopup = false;
+    },
+  },
 };
 </script>
+<style> .install-popup {
+   position: fixed;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   background: #fff;
+   padding: 20px;
+   border: 1px solid #ccc;
+   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+ }
+
+ .install-popup-content {
+   text-align: center;
+ }
+
+ .install-popup button {
+   margin: 10px;
+   padding: 8px 16px;
+   cursor: pointer;
+ }
+</style>
