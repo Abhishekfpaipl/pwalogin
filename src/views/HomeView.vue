0e5012d151f016d6bcf27133c48413d80423c5ec
Abhishekfpaipl <131144777+Abhishekfpaipl@users.noreply.github.com>
2023-12-02 15:19:38 +0530

    added axios

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Pwa Login Test" />
 
     <router-link to="/login" class="btn btn-primary">Login</router-link>
   </div>
