8973484630f7117b1e377b91c174ec04934495da
Abhishekfpaipl <131144777+Abhishekfpaipl@users.noreply.github.com>
2023-12-05 13:33:42 +0530

    test1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@
     </div>
 
     <HelloWorld msg="Welcome to Your Pwa Login Test" />
-    <p>Version 1.2.9</p>
+    <p>Version 1.3.0</p>
 
     <router-link to="/login" class="btn btn-primary">Login</router-link>
 
